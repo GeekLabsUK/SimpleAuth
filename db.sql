48652e70bba6d5617c5340045be0bd31dbd42228
Geeklabs <geeklabsonline@gmail.com>
2020-11-18 18:38:26 +0000

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 18, 2020 at 12:13 AM
+-- Generation Time: Nov 18, 2020 at 06:37 PM
 -- Server version: 5.7.19
 -- PHP Version: 7.4.9
 
@@ -18,8 +18,34 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `newadminset`
+-- Database: `simpleadmin`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_logins`
+--
+
+DROP TABLE IF EXISTS `auth_logins`;
+CREATE TABLE IF NOT EXISTS `auth_logins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(50) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `ip_address` varchar(255) NOT NULL,
+  `date` datetime NOT NULL,
+  `successfull` int(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `auth_logins`
+--
+
+INSERT INTO `auth_logins` (`id`, `user_id`, `firstname`, `lastname`, `role`, `ip_address`, `date`, `successfull`) VALUES
+(19, 1, 'super', 'admin', '2', '::1', '2020-11-18 12:37:30', 1);
 
 -- --------------------------------------------------------
 
@@ -60,7 +86,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `reset_token`, `reset_expire`, `activated`, `activate_token`, `activate_expire`, `role`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'super', 'admin', 'superadmin@admin.com', '$argon2id$v=19$m=65536,t=4,p=1$bjRmUGs5QkdFSzUuWG1MYw$jV+mMJB0xr7lSul9VHBxU1m8+X6lfFeyKba77mx6CJs', '', NULL, 1, NULL, NULL, 2, '2020-11-18 18:35:43', '2020-11-18 18:35:43', NULL);
 
 -- --------------------------------------------------------
 
